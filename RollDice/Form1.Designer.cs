4a3404b352c9288e8a557410a7113c62af7e75ff
emekteryen <emekteryen35@gmail.com>
2024-09-08 18:41:24 +0300

    ZarSallama
    C# ile çalışan zar sallama uygulaması. Windows Forms ile çalışmakta

---
+++ RollDice/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace RollDice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSayi1 = new System.Windows.Forms.Label();
+            this.lblSayi2 = new System.Windows.Forms.Label();
+            this.btnSalla = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblSayi1
+            // 
+            this.lblSayi1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayi1.Location = new System.Drawing.Point(12, 9);
+            this.lblSayi1.Name = "lblSayi1";
+            this.lblSayi1.Size = new System.Drawing.Size(98, 104);
+            this.lblSayi1.TabIndex = 0;
+            this.lblSayi1.Text = "0";
+            this.lblSayi1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSayi2
+            // 
+            this.lblSayi2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSayi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayi2.Location = new System.Drawing.Point(116, 9);
+            this.lblSayi2.Name = "lblSayi2";
+            this.lblSayi2.Size = new System.Drawing.Size(98, 104);
+            this.lblSayi2.TabIndex = 1;
+            this.lblSayi2.Text = "0";
+            this.lblSayi2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSalla
+            // 
+            this.btnSalla.Location = new System.Drawing.Point(12, 130);
+            this.btnSalla.Name = "btnSalla";
+            this.btnSalla.Size = new System.Drawing.Size(202, 39);
+            this.btnSalla.TabIndex = 2;
+            this.btnSalla.Text = "Zar Salla";
+            this.btnSalla.UseVisualStyleBackColor = true;
+            this.btnSalla.Click += new System.EventHandler(this.btnSalla_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 178);
+            this.Controls.Add(this.btnSalla);
+            this.Controls.Add(this.lblSayi2);
+            this.Controls.Add(this.lblSayi1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSayi1;
+        private System.Windows.Forms.Label lblSayi2;
+        private System.Windows.Forms.Button btnSalla;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RollDice/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RollDice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Random rnd = new Random();
+        int Sayi1, Sayi2;
+        int sayac = 1;
+        bool hileVarmi = false;
+
+
+        private void btnSalla_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            hileVarmi = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (hileVarmi)
+            {
+                lblSayi1.Text = "6";
+                lblSayi2.Text = "6";
+
+            }
+            else
+            {
+                Sayi1 = rnd.Next(1, 7);
+                Sayi2 = rnd.Next(1, 7);
+                lblSayi1.Text = Sayi1.ToString();
+                lblSayi2.Text = Sayi2.ToString();
+                
+            }
+            sayac++;
+            if (sayac > 20)
+            {
+                timer1.Stop();
+                sayac = 1;
+                hileVarmi = false;
+            }
+        }
+    }
+}
